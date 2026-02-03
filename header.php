b069c2346e018269ea030ceb238a20ceb18885a3
“romainbezombes-cloud” <romain.bezombes@mmibordeaux.com>
2026-02-03 15:45:56 +0100

    bec

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ $current_page = $_SERVER['SCRIPT_NAME'];
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>BEC</title>
+    <title>Bordeaux Etudiants Club</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
