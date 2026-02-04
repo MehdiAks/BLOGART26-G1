a74ebcb75f0a3007625ed12a73f610baacffb756
MehdiAks <afantastik041@gmail.com>
2026-02-04 17:28:17 +0100

    cdsxwvcv

--- BDD/BLOGART26.sql
+++ BDD/BLOGART26.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mer. 04 fév. 2026 à 10:41
+-- Généré le : mer. 04 fév. 2026 à 15:28
 -- Version du serveur : 8.0.44
 -- Version de PHP : 8.3.28
 
@@ -487,24 +487,6 @@ INSERT INTO `COMMENT` (`numCom`, `dtCreaCom`, `libCom`, `dtModCom`, `attModOK`, 
 
 -- --------------------------------------------------------
 
---
--- Structure de la table `COOKIE_CONSENT`
---
-
-CREATE TABLE `COOKIE_CONSENT` (
-  `numCon` int NOT NULL,
-  `ipAdresse` varchar(45) NOT NULL,
-  `choixConsent` enum('accepted','rejected') NOT NULL,
-  `dateCon` datetime DEFAULT CURRENT_TIMESTAMP,
-  `dateExpiration` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Déchargement des données de la table `COOKIE_CONSENT`
---
-
-INSERT INTO `COOKIE_CONSENT` (`numCon`, `ipAdresse`, `choixConsent`, `dateCon`, `dateExpiration`) VALUES
-(1, '::1', 'accepted', '2026-01-30 16:00:08', '2026-01-31 15:00:08');
 
 -- --------------------------------------------------------
 
@@ -929,13 +911,6 @@ ALTER TABLE `COMMENT`
   ADD KEY `FK_ASSOCIATION_2` (`numArt`),
   ADD KEY `FK_ASSOCIATION_3` (`numMemb`);
 
---
--- Index pour la table `COOKIE_CONSENT`
---
-ALTER TABLE `COOKIE_CONSENT`
-  ADD PRIMARY KEY (`numCon`),
-  ADD KEY `idx_ip` (`ipAdresse`),
-  ADD KEY `idx_expiration` (`dateExpiration`);
 
 --
 -- Index pour la table `EQUIPE`
